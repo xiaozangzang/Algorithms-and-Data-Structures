5b8ae9e11619408b7c20d560d9ff9c6a847fdbec
张志鹏 <1430051203@qq.com>
2017-09-05 22:50:59 +0800

    增加了字符串全排列算法实现

---
+++ 编程题/Main08.java
@@ -0,0 +1,56 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+/**
+ * 输入一个字符串,按字典序打印出该字符串中字符的所有排列。例如输入字符串abc,
+ * 则打印出由字符a,b,c所能排列出来的所有字符串abc,acb,bac,bca,cab和cba。
+ *
+ */
+public class Main08 {
+	
+	 public static ArrayList<String> Permutation(String str) {
+	        ArrayList<String> result = new ArrayList<String>() ;
+	        if(str==null || str.length()==0) { return result ; }
+	 
+	        char[] chars = str.toCharArray() ;
+	        TreeSet<String> temp = new TreeSet<>() ;
+	        Permutation(chars, 0, temp);
+	        result.addAll(temp) ;
+	        return result ;
+	    }
+	 
+	    public static void Permutation(char[] chars, int begin, TreeSet<String> result) {
+	        if(chars==null || chars.length==0 || begin<0 || begin>chars.length-1) { return ; }
+	 
+	        if(begin == chars.length-1) {
+	            result.add(String.valueOf(chars)) ;
+	        }else {
+	            for(int i=begin ; i<=chars.length-1 ; i++) {
+	                swap(chars, begin, i) ;
+	 
+	                Permutation(chars, begin+1, result);
+	 
+	                swap(chars, begin, i) ;
+	            }
+	        }
+	    }
+	 
+	    public static void swap(char[] x, int a, int b) {
+	        char t = x[a];
+	        x[a] = x[b];
+	        x[b] = t;
+	    }
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.nextLine();
+		List list = Main08.Permutation(str);
+		System.out.println(list);
+	}
+
+}
